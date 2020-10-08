ce3dcf66b8c764de444aed0d2795ba2906fae2fa
Justin Rhodes <jrhodes@ackmanndickenson.com>
2020-10-08 15:06:13 -0500

    fix storage url

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,6 +1,6 @@
 <a href="/" class="flex flex-col items-start mb-8">
     <h1 class="text-10xl font-raleway font-black uppercase"
-        style="background: url(http://mnrunclub.test/storage/duotone.jpg) center/cover; background-clip: text; color: transparent;line-height: 0.9;">
+        style="background: url(/storage/duotone.jpg) center/cover; background-clip: text; color: transparent;line-height: 0.9;">
         run<br />club
     </h1>
 </a>
